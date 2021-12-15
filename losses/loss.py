4813682c952653ffdc5a5939f095766f6dd5bbdc
vstark21 <chepurivishwas360@gmail.com>
2021-12-16 03:03:33 +0530

    Added Jaccard Loss

--- losses/loss.py
+++ losses/loss.py
@@ -3,6 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import segmentation_models_pytorch as smp
 
+class JaccardLoss(nn.Module):
+    def __init__(
+        self,
+        smooth=0,
+        from_logits=True,
+        eps=1e-7
+    ):
+        super().__init__()
+        self.eps = eps
+        self.smooth = smooth
+        self.from_logits = from_logits
+    
+    def __call__(self, preds, targets):
+        if self.from_logits:
+            preds = torch.sigmoid(preds)
+            
+        preds = torch.round(preds)
+        intersection = torch.logical_and(preds, targets).sum() + self.smooth
+        union = torch.logical_or(preds, targets).sum() + self.eps + self.smooth
+
+        return 1 - (intersection / union)
+
 class CloudLoss(nn.Module):
     def __init__(
         self, 
@@ -17,7 +39,7 @@ class CloudLoss(nn.Module):
 
         self.bce_loss = nn.BCEWithLogitsLoss()
         self.dice_loss = smp.losses.DiceLoss(mode='binary', smooth=1)
-        self.jacc_loss = smp.losses.JaccardLoss(mode='binary', smooth=1)
+        self.jacc_loss = JaccardLoss(smooth=1)
 
     def __call__(self, preds, targets, smooth=1):
         bce = self.bce_loss(preds, targets)
