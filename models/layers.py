13b9e28a4925c74f81e1cf0f81d078c3c2b02de2
vstark21 <chepurivishwas360@gmail.com>
2022-01-06 10:34:36 +0530

    Minor Changes

--- models/layers.py
+++ models/layers.py
@@ -263,17 +263,17 @@ class ResBlock(nn.Module):
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels, mid_channels=None, act_layer=nn.ReLU):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            act_layer(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            act_layer(inplace=True)
         )
 
     def forward(self, x):
