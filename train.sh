e597978be5457b6dd374ae3a22c63319b241a876
vstark21 <chepurivishwas360@gmail.com>
2022-01-18 16:53:40 +0530

    Minor Changes

---
+++ train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l select=ncpus=8:mem=16gb:ngpus=1 
+#PBS -q gpu                             
+
+module load cuda/10
+module load anaconda/3
+source activate virenv
+
+cd Cloud-Cover-Detection
+
+python main.py
+
+conda deactivate
+module unload anaconda/3
+module unload cuda
