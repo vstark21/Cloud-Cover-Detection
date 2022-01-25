ea94afbc3dac1109236169d88a6bbf4fe1d4dbe6
vstark21 <chepurivishwas360@gmail.com>
2022-01-25 17:56:14 +0530

    Changed submission.main

--- submission/main.py
+++ submission/main.py
@@ -11,7 +11,6 @@ import yaml
 import datetime
 import pandas as pd
 
-
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
@@ -20,38 +19,20 @@ class AttrDict(dict):
 with open("config.yml", "r") as f:
     config = AttrDict(yaml.safe_load(f))
 config.DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-config.DATA_MEAN = np.array(config.DATA_MEAN)
-config.DATA_STD = np.array(config.DATA_STD)
 
 ROOT_DIRECTORY = "/codeexecution"
 if not os.path.exists(ROOT_DIRECTORY):
     ROOT_DIRECTORY = "testexecution"
 PREDICTIONS_DIRECTORY = os.path.join(ROOT_DIRECTORY, "predictions")
-META_FILE_PATH = os.path.join(ROOT_DIRECTORY, "data")
 feature_directory = os.path.join(ROOT_DIRECTORY, "data", "test_features")
 
 chips = os.listdir(feature_directory)
-
 logger.info(f"Processing {len(chips)} chips in {feature_directory}")
-
 model = getattr(models, config.MODEL)(
     **config.MODEL_PARAMS[config.MODEL]
 ).to(config.DEVICE)
-
-model = model.to(config.DEVICE)
 model.load_state_dict(torch.load(F"{config.NAME}.pt", map_location=config.DEVICE))
 model.eval()
-
-meta_data = pd.read_csv(os.path.join(META_FILE_PATH, "test_metadata.csv"))
-locations = config.LOCATIONS
-chip_mapper = {
-    chip_id: [cur_loc, datetime.datetime.strptime(cur_dt, "%Y-%m-%dT%H:%M:%SZ")]
-        for chip_id, cur_loc, cur_dt in zip(
-            meta_data['chip_id'].values,
-            meta_data['location'].values,
-            meta_data['datetime'].values
-        )
-}
 
 @torch.no_grad()
 def main():
@@ -59,24 +40,20 @@ def main():
     for i, chip in enumerate(chips):
         files = [os.path.join(feature_directory, chip, file) 
                     for file in ["B02.tif", "B03.tif", "B04.tif", "B08.tif"]]
-        inputs = np.array(
-            [np.array(Image.open(filename)) for filename in files]
-        )
+        inputs = []
+        for filename in files:
+            _band = np.array(Image.open(filename))
+            key = os.path.split(filename)[-1].split(".")[0]
+            _band = (_band - config.DATA_MEAN[key]) / config.DATA_STD[key]
+            inputs.append(_band)
+
+        inputs = np.stack(inputs, axis=0)
         inputs = inputs.astype(np.float32)
-        inputs = inputs - config.DATA_MEAN[:, np.newaxis, np.newaxis]
-        inputs = inputs / config.DATA_STD[:, np.newaxis, np.newaxis]
         inputs = inputs[np.newaxis, :, :, :]
         inputs = torch.Tensor(inputs)
         inputs = inputs.to(config.DEVICE)
 
-        cur_loc, cur_dt = chip_mapper[chip]
-        meta = np.zeros(len(locations) + 12)
-        meta[locations.index(cur_loc)] = 1
-        meta[len(locations) + cur_dt.month - 1] = 1
-        meta = torch.Tensor(meta[np.newaxis, :])
-        meta = meta.to(config.DEVICE)
-
-        pred = model(inputs, meta)['out']
+        pred = model(inputs)['out']
         pred = torch.sigmoid(pred).squeeze().cpu().numpy()
         pred = np.round(pred).astype(np.uint8)
 
