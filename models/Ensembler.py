1b6077ff83f3f8b455a678140314ae7f6846f1b6
vstark21 <chepurivishwas360@gmail.com>
2022-02-01 14:27:31 +0530

    Debugging Ensembler

--- models/Ensembler.py
+++ models/Ensembler.py
@@ -2,17 +2,19 @@ import torch
 import torch.nn as nn
 
 class Nugget(nn.Module):
-    def __init__(self, kernel_size, in_channels, out_channels, padding=0):
+    def __init__(self, in_channels, out_channels, kernel_size, use_act=True):
         super(Nugget, self).__init__()
         self.conv2d = nn.Conv2d(in_channels,
                                 out_channels,
-                                kernel_size=kernel_size,
-                                padding=padding)
-        self.act = nn.SiLU()
+                                kernel_size=kernel_size)
+        self.act = None
+        if use_act:
+            self.act = nn.SiLU()
 
     def forward(self, x):
         x = self.conv2d(x)
-        x = self.act(x)
+        if self.act:
+            x = self.act(x)
         return x
 
 class Ensembler(nn.Module):
@@ -23,13 +25,17 @@ class Ensembler(nn.Module):
     ):
         super(Ensembler, self).__init__()
         self.nuggets = nn.Sequential(
-            Nugget(3, in_channels, 16, padding=1),
-            Nugget(1, 16, in_channels),
+            Nugget(in_channels, 16, 1),
+            Nugget(16, in_channels, 1, use_act=False),
         )
+        self.act = nn.SiLU()
         self.out_conv = nn.Conv2d(in_channels, out_channels, 1)
         
     def forward(self, x):
+        identity = x
         x = self.nuggets(x)
+        x += identity
+        x = self.act(x)
         out = self.out_conv(x)
 
         return dict(out=out)
